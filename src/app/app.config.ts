c4da1978865d98253aae5200782c56a82f7f5e66
Karanam Ganesh <ganeshkaranam629@gmail.com>
2026-07-23 11:49:17 +0530

    Add JWT auth interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,29 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection
+} from '@angular/core';
 
+import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
-import { provideHttpClient } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptors
+} from '@angular/common/http';
+
+import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideZoneChangeDetection({
+      eventCoalescing: true
+    }),
     provideRouter(routes),
-    provideHttpClient()
+
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    )
   ]
 };
